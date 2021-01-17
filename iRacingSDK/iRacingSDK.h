dd8d7e78e1623d8a952f573b19f812569554997c
Socii Watts <sociiw@gmail.com>
2021-01-17 18:27:10 +0000

    Initial Commit

---
+++ iRacingSDK/iRacingSDK.h
@@ -0,0 +1,16 @@
+// iRacingSDK.h
+
+// Copyright © 2021 Socii. All rights reserved.
+// MIT License @ end of file.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iRacingSDK.
+FOUNDATION_EXPORT double iRacingSDKVersionNumber;
+
+//! Project version string for iRacingSDK.
+FOUNDATION_EXPORT const unsigned char iRacingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iRacingSDK/PublicHeader.h>
+
+
